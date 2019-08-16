a9736bb7d2dd55b32963bc975c8696ea3952349a
kpcruz <kelvinmembership@gmail.com>
2019-08-16 00:03:58 -0700

    able to query Yelp api

---
+++ src/api/yelp.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+require("dotenv").config();
+
+export default axios.create({
+	baseURL: "https://api.yelp.com/v3/businesses",
+	headers: {
+		Authorization: `Bearer ${process.env.APIKEY}`
+	}
+});
+
+// yelp.get();
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,18 +1,33 @@
 import React, { useState } from "react";
 import { View, Text, Stylesheet } from "react-native";
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
+	const [results, setResults] = useState([]);
+
+	const searchApi = async () => {
+		const response = await yelp.get("/search", {
+			params: {
+				limit: 50,
+				term,
+				location: "San Jose"
+			}
+		});
+		setResults(response.data.businesses);
+	};
 
 	return (
 		<View>
 			<SearchBar
 				term={term}
-				onTermChange={newTerm => setTerm(newTerm)}
-				onTermSubmit={() => console.log("termSubmit")}
+				onTermChange={setTerm}
+				// onTermChange={newTerm => setTerm(newTerm)}
+				onTermSubmit={() => searchApi()}
 			/>
 			<Text>Search Screen </Text>
+			<Text>We have found {results.length}</Text>
 		</View>
 	);
 };
