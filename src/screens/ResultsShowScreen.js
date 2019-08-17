5df2776754759b1f47457609c46ec6e5137d48d3
kpcruz <kelvinmembership@gmail.com>
2019-08-17 00:40:53 -0700

    fixed results from not showing pictures. Had to figure which was an object to pass

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,18 +1,34 @@
-import React, {useState} from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
-import yelp from '../api/yelp'
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Image, FlatList } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsList = ({ navigation }) => {
-    const [result, setResult] = useState(null);
-    const id = navigation.getParam("id");
-    
-    const getResult=(id) => {
-        const response = await yelp.get(`/${id}`);
-        response.data;
-    }
+	const [result, setResult] = useState(null);
+	const id = navigation.getParam("id");
+
+	const getResult = async id => {
+		const response = await yelp.get(`/${id}`);
+		setResult(response.data);
+	};
+
+	useEffect(() => {
+		getResult(id);
+	}, []);
+
+	if (!result) {
+		return null;
+	}
+
 	return (
 		<View>
-			<Text>Results Show</Text>
+			<Text>{result.name}</Text>
+			<FlatList
+				data={result.photos}
+				keyExtractor={photo => photo}
+				renderItem={({ item }) => {
+					return <Image style={styles.image} source={{ uri: item }} />;
+				}}
+			/>
 		</View>
 	);
 };
@@ -20,6 +36,10 @@ const ResultsList = ({ navigation }) => {
 const styles = StyleSheet.create({
 	container: {
 		marginLeft: 15
+	},
+	image: {
+		height: 200,
+		width: 300
 	}
 });
 
