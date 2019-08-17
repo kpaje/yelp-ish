fb434f8179f6e6653fbdd232cdefa8ccc1cb2db4
kpcruz <kelvinmembership@gmail.com>
2019-08-16 23:09:42 -0700

    able to produce results from yelp api such as pictures, stars, and review count. Nested as SearchSCreen>ResultList>ResultDetail

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Stylesheet } from "react-native";
-import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
 
 export default () => {
